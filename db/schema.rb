b220ce01d1108f8a9d745c47540238412fb01ec5
omnibot3000 <omnibot3000@pop-os.localdomain>
2020-08-24 22:43:13 -0400

    removed author column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_023926) do
+ActiveRecord::Schema.define(version: 2020_08_05_023247) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2020_08_05_023926) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "author"
   end
 
 end
